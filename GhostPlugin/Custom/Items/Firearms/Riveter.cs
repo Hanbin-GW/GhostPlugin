fb4b99f1ed4d936bd7eb56e709b23fdfb8ff3fe2
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-03-28 16:12:03 +0900

    Set a spawn position on `Riverter.cs`

--- GhostPlugin/Custom/Items/Firearms/Riveter.cs
+++ GhostPlugin/Custom/Items/Firearms/Riveter.cs
@@ -1,10 +1,14 @@
+using System.Collections.Generic;
+using Exiled.API.Enums;
 using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Item;
 using Exiled.Events.EventArgs.Player;
 using UnityEngine;
 using YamlDotNet.Serialization;
 using GhostPlugin.Methods.Objects;
+using InventorySystem.Items.Firearms.Attachments;
 
 namespace GhostPlugin.Custom.Items.Firearms
 {
@@ -15,12 +19,53 @@ namespace GhostPlugin.Custom.Items.Firearms
         public override string Name { get; set; } = "Riveter";
         public override string Description { get; set; } = "AR 이지만 <color=#ebc934>12게이지 용의 숨결</color> 을 사용하는 연발 샷건입니다.";
         public override float Weight { get; set; } = 7.5f;
-        public override SpawnProperties SpawnProperties { get; set; }
+
+        public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties()
+        {
+            RoomSpawnPoints = new List<RoomSpawnPoint>()
+            {
+                new RoomSpawnPoint()
+                {
+                    Room = RoomType.Hcz079,
+                    Chance = 100,
+                },
+                new RoomSpawnPoint()
+                {
+                    Room = RoomType.HczNuke,
+                    Chance = 50,
+                }
+            }
+        };
         public override byte ClipSize { get; set; } = 15;
         public override ItemType Type { get; set; } = ItemType.GunE11SR;
         [YamlIgnore] 
         public override float Damage { get; set; }
+
+        public override AttachmentName[] Attachments { get; set; } = new AttachmentName[]
+        {
+            AttachmentName.LowcapMagJHP
+        };
         
+        private void OnChangingAttachments(ChangingAttachmentsEventArgs ev)
+        {
+            if (!Check(ev.Player.CurrentItem))
+                return;
+            ev.IsAllowed = false;
+            ev.Player.ShowHint("이 아이탬은 부착물 변경이 금지되어있습니다!", 3);
+        }
+
+        protected override void SubscribeEvents()
+        {
+            Exiled.Events.Handlers.Item.ChangingAttachments += OnChangingAttachments;
+            base.SubscribeEvents();
+        }
+
+        protected override void UnsubscribeEvents()
+        {
+            Exiled.Events.Handlers.Item.ChangingAttachments -= OnChangingAttachments;
+            base.UnsubscribeEvents();
+        }
+
         protected override void OnShot(ShotEventArgs ev)
         {
             if (Check(ev.Player.CurrentItem))
