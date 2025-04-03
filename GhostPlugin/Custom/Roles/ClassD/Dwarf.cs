03140c40d61bc5ec71e0b87e7b9361bf0185ad35
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-04-03 14:56:03 +0900

    Dwarf.cs

---
+++ GhostPlugin/Custom/Roles/ClassD/Dwarf.cs
@@ -0,0 +1,31 @@
+using Exiled.API.Features;
+using Exiled.CustomRoles.API.Features;
+using GhostPlugin.API;
+using MEC;
+using UnityEngine;
+
+namespace GhostPlugin.Custom.Roles.ClassD
+{
+    public class Dwarf : CustomRole, ICustomRole
+    {
+        public override uint Id { get; set; } = 15;
+        public override int MaxHealth { get; set; } = 50;
+        public override string Name { get; set; } = "Dwarf";
+        public override string Description { get; set; } = "매우 작은 외형의 D계급 인원입니다.";
+        public override string CustomInfo { get; set; } = "Dwarf";
+        public StartTeam StartTeam { get; set; } = StartTeam.ClassD;
+        public int Chance { get; set; } = 70;
+
+        protected override void RoleAdded(Player player)
+        {
+            Timing.CallDelayed(0.5f, () => player.Scale = new Vector3(0.4f, 0.4f, 0.4f));
+            base.RoleAdded(player);
+        }
+
+        protected override void RoleRemoved(Player player)
+        {
+            Timing.CallDelayed(0.1f, () => player.Scale = Vector3.one);
+            base.RoleRemoved(player);
+        }
+    }
+}
