9c8e3fdf822abb505aa24dd4514da2e4b1c3b63c
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-04-05 16:26:49 +0900

    `StopMusic.cs`: Stops the Music

---
+++ GhostPlugin/Commands/MusicCommand/StopMusic.cs
@@ -0,0 +1,28 @@
+using System;
+using CommandSystem;
+using GhostPlugin.Methods.Music;
+
+namespace GhostPlugin.Commands.MusicCommand
+{
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class StopMusic : ICommand
+    {
+        public string Command => "StopMusic";
+        public string[] Aliases { get; } =  new []{"sm"};
+        public string Description => "현재 재생 중인 음악을 중지합니다.";
+        public MusicManager MusicManager = new MusicManager();
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            if (Plugin.Instance == null)
+            {
+                response = "현재 재생 중인 음악이 없습니다.";
+                return false;
+            }
+
+            // 음악을 중지하는 로직 호출
+            MusicManager.StopMusic();
+            response = "음악이 중지되었습니다.";
+            return true;
+        }
+    }
+}
