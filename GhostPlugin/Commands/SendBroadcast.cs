a697ce617f21462128f7822db71ed3c5971d3ea9
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-04-09 13:15:52 +0900

    Broadcast Command fix :3

--- GhostPlugin/Commands/SendBroadcast.cs
+++ GhostPlugin/Commands/SendBroadcast.cs
@@ -19,11 +19,19 @@ namespace GhostPlugin.Commands
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             var player = Player.Get(sender);
-            string message = String.Empty;
-            string message_Argumanet = arguments.At(0).ToLower();
-            string message_Color = arguments.At(1).ToLower();
-            string message_Size = arguments.At(2).ToLower();
-            string duration = arguments.At(3).ToLower();
+            
+            if (arguments.Count < 4)
+            {
+                response = "사용법: SendBroadcast <메시지> <색깔> <텍스트 사이즈> <지속 시간>";
+                return false;
+            }
+
+            string message_Argumanet = arguments.At(0);
+            string message_Color = arguments.At(1);
+            string message_Size = arguments.At(2);
+            string duration = arguments.At(3);
+            
+            string message = $"<size={message_Size}><color={message_Color}>{message_Argumanet}</color></size>";
             if (player == null)
             {
                 response = "플레이어를 찾을 수 없습니다.";
@@ -36,18 +44,12 @@ namespace GhostPlugin.Commands
                     message_Size, ushort.Parse(duration));
             }
 
-            if (arguments.Count < 3)
-            {
-                response = "사용법: SendBroadcast <메시지> <색깔> <택스트 사이즈>";
-                return false;
-            }
-
             foreach (Player players in Player.List)
             {
                 players.Broadcast(5,message);
             }
-            message = $"<size={message_Size}><color={message_Color}>{message_Argumanet}</color></size>";
-            response = $"메시지가 모든 유저한테 전송되었습니다:      {message}";
+
+            response = $"메시지가 모든 유저에게 전송되었습니다: {message}";
             return true;
         }
         public string Command { get; } = "SendBroadcast";
