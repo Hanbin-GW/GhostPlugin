8faa9b482810c4fca1c6f23c6c063ede89518d14
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-04-29 10:46:08 +0900

    Add a MER methods

---
+++ GhostPlugin/Methods/MER/ObjectManager.cs
@@ -0,0 +1,31 @@
+using System;
+using Exiled.API.Features;
+using ProjectMER.Features;
+using ProjectMER.Features.Objects;
+using UnityEngine;
+
+namespace GhostPlugin.Methods.MER
+{
+    public class ObjectManager
+    {
+        public void SpawnObject(String schematicName, Vector3 spawnPos, Vector3 rotation)
+        {
+            SchematicObject schematicObject = ObjectSpawner.SpawnSchematic(schematicName, spawnPos, rotation);
+            if (schematicObject != null)
+            {
+                Log.Debug($"Schematic '{schematicName}' has been successfully spawned.");
+                GameObject schematicGameObject = schematicObject.gameObject;
+                Rigidbody rigidbody = schematicGameObject.AddComponent<Rigidbody>();
+                rigidbody.useGravity = true;
+            }
+        }
+
+        public void RemoveObject(SchematicObject schematicObject)
+        {
+            if (schematicObject != null && schematicObject.gameObject != null)
+            {
+                UnityEngine.Object.Destroy(schematicObject.gameObject);
+            }
+        }
+    }
+}
