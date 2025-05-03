ecef08ecc62201164193962ca29e64cd8bd6865c
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-03 22:57:00 +0900

    Set an ImpactGrenade in `SpawnProperties()`

--- GhostPlugin/Custom/Items/Grenades/ImpactGrenade.cs
+++ GhostPlugin/Custom/Items/Grenades/ImpactGrenade.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using Exiled.API.Enums;
 using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
@@ -12,7 +14,39 @@ namespace GhostPlugin.Custom.Items.Grenades
         public override string Description { get; set; } = "충격시 즉시 폭발하는 수류탄입니다!";
         public override float Weight { get; set; } = 3.5f;
         public override ItemType Type { get; set; } = ItemType.GrenadeHE;
-        public override SpawnProperties SpawnProperties { get; set; }
+
+        public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties()
+        {
+            Limit = 4,
+            DynamicSpawnPoints = new List<DynamicSpawnPoint>()
+            {
+                new DynamicSpawnPoint()
+                {
+                    Location = SpawnLocationType.Inside939Cryo,
+                    Chance = 45,
+                },
+                new DynamicSpawnPoint()
+                {
+                    Location = SpawnLocationType.Inside939Cryo,
+                    Chance = 30,
+                },
+                new DynamicSpawnPoint()
+                {
+                    Location = SpawnLocationType.InsideHidChamber,
+                    Chance = 30,
+                },
+                new DynamicSpawnPoint()
+                {
+                    Chance = 50,
+                    Location = SpawnLocationType.InsideLczArmory
+                },
+                new DynamicSpawnPoint()
+                {
+                    Chance = 75,
+                    Location = SpawnLocationType.InsideLczArmory
+                }
+            }
+        };
         public override bool ExplodeOnCollision { get; set; } = true;
         public override float FuseTime { get; set; } = 4.5f;
     }
