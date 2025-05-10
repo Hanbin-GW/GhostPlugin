c98f7414f7e62b1c9ed3cd998418481f3a6a275f
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-10 17:12:10 +0900

    오타 및 맞춤법 수정

--- GhostPlugin/Configs/ServerEventsConfigs/NoobSupportConfig.cs
+++ GhostPlugin/Configs/ServerEventsConfigs/NoobSupportConfig.cs
@@ -9,22 +9,22 @@ namespace GhostPlugin.Configs.ServerEventsConfigs
 
         public bool ShowHintOnEquipItem { get; set; }
 
-        public string Scp207HintMessage { get; set; } = "당신은 {0} 개의 SCP-207 를 마셧습니다!";
+        public string Scp207HintMessage { get; set; } = "당신은 {0} 개의 SCP-207 를 마셨습니다!";
 
-        public string AntiScp207HintMessage { get; set; } = "당신은 {0} 개의 Anti SCP-207 를 마셧습니다!";
+        public string AntiScp207HintMessage { get; set; } = "당신은 {0} 개의 Anti SCP-207 를 마셨습니다!";
 
         public string CardiacArrestMessage { get; set; } = "심정지 가 감지되었습니다.. 대량의 의료용 아이탬 또는 아드레난린 주사기를 사용하여 회복하십시요..!";
 
         public string PoisonMessage { get; set; } = "당신은 중독되었습니다..!\n<color=green>SCP-500</color> 을 사용하여 독을 완화하십시요...!";
         public string LowHpMessage { get; set; } = "HP 가 매우낮습니다...\n가능한 빨리 의료장비를 사용하십시요..!";
 
-        public string A7Info { get; set; } = "A7 에 피격당하셧습니다...\nA7 의 피격시 화상효과가 일어납니다..";
+        public string A7Info { get; set; } = "A7 에 피격당하셨습니다...\nA7 의 피격시 화상효과가 일어납니다..";
 
         public string Looking096 { get; set; } = "SCP096 이 당신을 추적하고있습니다..!\n폭주가 끝날때까지 버티십시요..!";
 
         public string JailbirdUseMessage { get; set; } = "남은 돌진공격 수: {0}";
 
-        public string ScpHealMessage { get; set; } = "적을 처치하여 {0} 의 HP 를 회복하셧습니다.";
+        public string ScpHealMessage { get; set; } = "적을 처치하여 {0} 의 HP 를 회복하셨습니다.";
 
         public string BleedingMessage { get; set; } = "출혈이 감지되었습니다..\n의료용 아이탬을 사용하십시요..!";
 
@@ -33,10 +33,10 @@ namespace GhostPlugin.Configs.ServerEventsConfigs
         public int DelayLczMessageTime { get; set; } = 18;
 
         public string Lcz15_hint { get; set; } =
-            "저위험군에서 나갈수 없으신가요?\n키카드를 얻어서 914에서 업그래이드 하세요\n O5 같은 권한이 높은 키카드를 얻으셧다면 바로 나가셔도 됩니다!";
+            "저위험군에서 나갈수 없으신가요?\n키카드를 얻어서 914에서 업그래이드 하세요\n O5 같은 권한이 높은 키카드를 얻으셨다면 바로 나가셔도 됩니다!";
 
         public string Lcz10_hint { get; set; } =
-            "저위험군 폐쇠까지 10분 남았습니다!\n키카드를 얻어서 914에서 업그래이드 하세요\n O5 같은 권한이 높은 키카드를 얻으셧다면 바로 나가셔도 됩니다!";
+            "저위험군 폐쇠까지 10분 남았습니다!\n키카드를 얻어서 914에서 업그래이드 하세요\n O5 같은 권한이 높은 키카드를 얻셨셨다면 바로 나가셔도 됩니다!";
 
         public string Lcz5_hint { get; set; } = "저위험군 폐쇠까지 5분 남았습니다!\n슬슬 나갈 준비를 해야합니다..";
 
