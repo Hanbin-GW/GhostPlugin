469003128119bb255eb16f50107e1936907df14f
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-11 22:58:53 +0900

    신규직업: 전략가, 보급관

---
+++ GhostPlugin/Custom/Roles/Chaos/Quartermaster.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using Exiled.API.Enums;
+using Exiled.API.Features.Attributes;
+using Exiled.CustomRoles.API.Features;
+using GhostPlugin.API;
+using PlayerRoles;
+
+namespace GhostPlugin.Custom.Roles.Chaos
+{
+    [CustomRole(RoleTypeId.ChaosConscript)]
+    public class Quartermaster : CustomRole, ICustomRole
+    {
+        public override uint Id { get; set; } = 20;
+        public override int MaxHealth { get; set; } = 100;
+        public override string Name { get; set; } = "Quartermaster (보급관)";
+        public override string Description { get; set; } = "혼돈의반란 보급병입니다!";
+        public override string CustomInfo { get; set; } = "Quartermaster";
+        public StartTeam StartTeam { get; set; } = StartTeam.Chaos;
+        public int Chance { get; set; } = 80;
+        public override RoleTypeId Role { get; set; } = RoleTypeId.ChaosRifleman;
+
+        public override List<string> Inventory { get; set; } = new List<string>()
+        {
+            ItemType.KeycardChaosInsurgency.ToString(),
+            ItemType.GunAK.ToString(),
+            18.ToString(),
+            18.ToString(),
+            ItemType.ArmorCombat.ToString(),
+            ItemType.Adrenaline.ToString(),
+            ItemType.Painkillers.ToString(),
+        };
+
+        public override Dictionary<AmmoType, ushort> Ammo { get; set; } = new Dictionary<AmmoType, ushort>()
+        {
+            { AmmoType.Nato762, 90 },
+        };
+    }
+}
---
+++ GhostPlugin/Custom/Roles/Chaos/Strategist.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using Exiled.API.Features.Attributes;
+using Exiled.CustomRoles.API.Features;
+using GhostPlugin.API;
+using GhostPlugin.Custom.Abilities.Active;
+using PlayerRoles;
+
+namespace GhostPlugin.Custom.Roles.Chaos
+{
+    [CustomRole(RoleTypeId.ChaosMarauder)]
+    public class Strategist : CustomRole, ICustomRole
+    {
+        public override uint Id { get; set; } = 19;
+        public override int MaxHealth { get; set; } = 120;
+        public override string Name { get; set; } = "Strategist (전략가)";
+        public override string Description { get; set; } = "전투에서 다양한 전략으로 많은 전장을 승리로 이끌고온 혼돈읩 반란 대장입니다!";
+        public override string CustomInfo { get; set; } = "Strategist";
+        public StartTeam StartTeam { get; set; } = StartTeam.Chaos;
+        public int Chance { get; set; } = 90;
+        public override bool DisplayCustomItemMessages { get; set; } = false;
+
+        public override List<string> Inventory { get; set; } = new List<string>()
+        {
+            ItemType.KeycardChaosInsurgency.ToString(),
+            ItemType.ArmorCombat.ToString(),
+            19.ToString(),
+            9.ToString(),
+            ItemType.Medkit.ToString(),
+            ItemType.Radio.ToString()
+        };
+
+        public override List<CustomAbility> CustomAbilities { get; set; } = new List<CustomAbility>()
+        {
+            new EnhancedGoggleVision()
+            {
+                Name = "향상된 고글 비전",
+                Description = "일시적으로 벽넘어의 적을 볼수 있습니다!"
+            },
+        };
+    }
+}
