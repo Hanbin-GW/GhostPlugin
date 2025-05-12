33e725733c6abbed7e52f7fd28d9eaae518103c4
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-12 10:13:39 +0900

    Reset the ChiefScientist.cs inventory

--- GhostPlugin/Custom/Roles/Scientist/ChiefScientist.cs
+++ GhostPlugin/Custom/Roles/Scientist/ChiefScientist.cs
@@ -13,13 +13,16 @@ namespace GhostPlugin.Custom.Roles.Scientist
     {
         public override uint Id { get; set; } = 1;
         public override int MaxHealth { get; set; } = 100;
-        public override string Name { get; set; } = "<color=#ffd900>Chief Scientist</color>";
-        public override string Description { get; set; } = "SCP 제단의 총연구 책임자이며 대부분의 제단의 특수무기를 제작한사람\n특수아이탬이 제공되었습니다.";
+        public override string Name { get; set; } = "<color=#ffd900>Senior Scientist</color>";
+        public override string Description { get; set; } = "SCP 제단의 총연구 책임자이며 대부분의 제단의 특수무기를 제작한사람입니다!";
         public override string CustomInfo { get; set; } = "Chief Scientist";
         public override RoleTypeId Role { get; set; } = RoleTypeId.Scientist;
         public StartTeam StartTeam { get; set; } = StartTeam.Scientist;
         public int Chance { get; set; } = 100;
         public override bool DisplayCustomItemMessages { get; set; } = false;
+        public override bool KeepRoleOnChangingRole { get; set; } = true;
+        public override bool KeepRoleOnDeath { get; set; } = false;
+
         public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties()
         {
             Limit = 1,
@@ -28,16 +31,9 @@ namespace GhostPlugin.Custom.Roles.Scientist
         public override List<string> Inventory { get; set; } = new List<string>()
         {
             ItemType.KeycardResearchCoordinator.ToString(),
-            28.ToString(),
-            24.ToString(),
-            11.ToString(),
+            ItemType.SCP500.ToString(),
+            ItemType.SCP268.ToString(),
+            ItemType.SCP207.ToString(),
         };
-
-        private void OnEscaped(EscapedEventArgs ev)
-        {
-            if (Check(ev.Player))
-            {
-            }
-        }
     }
 }
