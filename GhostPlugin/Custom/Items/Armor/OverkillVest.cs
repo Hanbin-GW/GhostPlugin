3f49695cda0cdba7e274630d10cb06699ffd427a
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-20 08:51:02 +0900

    오버킬 조끼 비활성화!

--- GhostPlugin/Custom/Items/Armor/OverkillVest.cs
+++ GhostPlugin/Custom/Items/Armor/OverkillVest.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Exiled.API.Extensions;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomItems.API.Features;
-using Exiled.Events.EventArgs.Player;
-using InventorySystem;
-using InventorySystem.Items;
-using MEC;
+using Exiled.CustomRoles.API;
 
 namespace GhostPlugin.Custom.Items.Armor
 {
@@ -21,66 +14,10 @@ namespace GhostPlugin.Custom.Items.Armor
         public override float Weight { get; set; } = 3f;
         public override SpawnProperties SpawnProperties { get; set; }
         public override ItemType Type { get; set; } = ItemType.ArmorCombat;
-        private readonly Dictionary<Player, CoroutineHandle> _altKeyCooldowns =
-            new Dictionary<Player, CoroutineHandle>();
-        private void GiveRandomWeapon(Player player)
-        {
-            ItemType[] weaponTypes = Enum.GetValues(typeof(ItemType))
-                .Cast<ItemType>()
-                .Where(x => x.IsWeapon())
-                .ToArray();
-            ItemType randomWeapon = weaponTypes[UnityEngine.Random.Range(0, weaponTypes.Length)];
-            ItemBase newItem = player.Inventory.ServerAddItem(randomWeapon, ItemAddReason.AdminCommand);
-            ushort serial = newItem.ItemSerial;
-            player.Inventory.ServerSelectItem(serial);
-            //player.AddItem(randomWeapon);
-            player.ShowHint($"오버킬 능력으로 {randomWeapon} 을 받았습니다!", 5);
-
-        }
-
-        private IEnumerator<float> CooldownCorutine(Player player)
-        {
-            yield return Timing.WaitForSeconds(60f);
-            _altKeyCooldowns.Remove(player);
-            player.ShowHint("쿨다운이 끝났습니다.\n능력을 사용할수 있습니다.", 5);
-        }
-
-        private void OnTogglingNoClip(TogglingNoClipEventArgs ev)
-        {
-            if (Check(ev.Player.CurrentArmor))
-            {
-                if (!ev.IsAllowed)
-                {
-                    if (_altKeyCooldowns.ContainsKey(ev.Player))
-                    {
-                        ev.Player.ShowHint("쿨다운이 아직 진행중입니다..\n능력을 사용할수 없습니다..", 5);
-                    }
-                    else
-                    {
-                        CoroutineHandle handle = Timing.RunCoroutine(CooldownCorutine(ev.Player));
-                        _altKeyCooldowns[ev.Player] = handle;
-                        GiveRandomWeapon(ev.Player);
-                    }
-                }
-            }
-        }
 
         protected override void OnAcquired(Player player, Item item, bool displayMessage)
         {
-            Timing.CallDelayed(7, ()=>player.ShowHint(Name + $"\n{Description}",5));
             base.OnAcquired(player, item, displayMessage);
-        }
-
-        protected override void SubscribeEvents()
-        {
-            Exiled.Events.Handlers.Player.TogglingNoClip += OnTogglingNoClip;
-            base.SubscribeEvents();
-        }
-
-        protected override void UnsubscribeEvents()
-        {
-            Exiled.Events.Handlers.Player.TogglingNoClip -= OnTogglingNoClip;
-            base.UnsubscribeEvents();
         }
     }
 }
