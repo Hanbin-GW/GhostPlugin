744012f1d2f3bdd3cce1d77ac8cfa63c588c06aa
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-19 23:10:43 +0900

    Update Exiled Version (test branch)

--- GhostPlugin/Plugin.cs
+++ GhostPlugin/Plugin.cs
@@ -27,7 +27,7 @@ namespace GhostPlugin
         /// Speakers List
         /// </summary>
         public int CurrentId = 1;
-        public override Version Version { get; } = new(4, 1,7,800);
+        public override Version Version { get; } = new(4, 1,8,800);
         public override string Author { get; } = "Hanbin-GW";
         public override string Name { get; } = "Ghost-Plugin";
         public override PluginPriority Priority { get; } = PluginPriority.Low;
