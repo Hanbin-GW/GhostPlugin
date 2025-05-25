a9078856149481bf067bcadad33a745d744a945f
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-25 21:15:46 +0900

    O5 관리자 탈출 해택 추가!

--- GhostPlugin/Custom/Roles/Scientist/O5Administrator.cs
+++ GhostPlugin/Custom/Roles/Scientist/O5Administrator.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using Exiled.API.Enums;
+using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
 using Exiled.API.Features.Spawn;
 using Exiled.CustomRoles.API.Features;
@@ -14,7 +16,7 @@ namespace GhostPlugin.Custom.Roles.Scientist
         public override bool DisplayCustomItemMessages { get; set; } = false;
         public override uint Id { get; set; } = 7;
         public override int MaxHealth { get; set; } = 100;
-        public override string Name { get; set; } = "<color=#ffc2b0>O5 Administrator</color>";
+        public override string Name { get; set; } = "<color=#ffc2b0>O5 관리자</color>";
         public override string Description { get; set; } = "당신은 O5 관리자입니다!";
         //public override string Description { get; set; } = "당신은 O5 관리자입니다!\n시설을 탈출하여 강화부대(Reinforcements MTF)한테 구조 신호를 요청하십시요!!!!";
         public override string CustomInfo { get; set; } = "O5 Administrator";
@@ -35,9 +37,20 @@ namespace GhostPlugin.Custom.Roles.Scientist
         };
         private void OnEscaped(EscapedEventArgs ev)
         {
-            if (Check(ev.Player))
+            if (Check(ev.Player) && ev.EscapeScenario == EscapeScenario.Scientist)
             {
                 Reinforcements.Plugin.Instance.IsSpawnable = true;
+
+                Respawn.ModifyTokens(Faction.FoundationStaff, +3);
+                Respawn.AdvanceTimer(Faction.FoundationStaff, -10);
+                Respawn.AdvanceTimer(Faction.FoundationEnemy, +5);
+            }
+
+            if (Check(ev.Player) && ev.EscapeScenario == EscapeScenario.CuffedScientist)
+            {
+                Respawn.ModifyTokens(Faction.FoundationEnemy, +3);
+                Respawn.AdvanceTimer(Faction.FoundationEnemy, -10);
+                Respawn.AdvanceTimer(Faction.FoundationStaff, +5);
             }
         }
 
