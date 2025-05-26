f30d72b3c601844254b10ad71738914b7fd48dcf
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-26 10:45:33 +0900

    ClassicConfig.cs: Translated eng

--- GhostPlugin/Configs/ServerEventsConfigs/ClassicConfig.cs
+++ GhostPlugin/Configs/ServerEventsConfigs/ClassicConfig.cs
@@ -6,6 +6,7 @@ namespace GhostPlugin.Configs.ServerEventsConfigs
     public class ClassicConfig
     {
         public bool OnEnabled { get; set; } = true;
+
         /// <summary>
         /// Ghost Plugin Classic Config
         /// </summary>
@@ -17,54 +18,64 @@ namespace GhostPlugin.Configs.ServerEventsConfigs
             Override = false
         };
 
-        [Description("Enable The SafeMode?")] 
-        public bool IsSafeMode { get; set; } = false;
-        [Description("라운드 시작 message")]
-        public string RoundStartMSG { get; set; } = "<size=38>👻라운드가 시작되었습니다...👻</size>\n<size=30>Good Luck</size>";
+        [Description("Enable The SafeMode?")] public bool IsSafeMode { get; set; } = false;
+
+        [Description("Round Start message")]
+        public string RoundStartMSG { get; set; } =
+            "<size=38>👻The Round has been started...👻</size>\n<size=30>Good Luck</size>";
+
         [Description("Reminder: This is imcomplete!")]
         public bool IsEnableFPSmap { get; set; } = false;
-        [Description("저위험군 봉쇄 15분 메세지")] public string Lcz15 { get; set; } = "저위험군 봉쇄 15분 남음.";
+
+        [Description("저위험군 봉쇄 15분 메세지")]
+        public string Lcz15 { get; set; } = "<color=red>Alert</color>\n<size=30>LCZ lockdown 15min left</size>";
 
         [Description("저위험군 봉쇄 15분 메세지 시간(초단위)")]
         public ushort Lcz15Time { get; set; } = 10;
 
-        [Description("저위험군 봉쇄 10분 메세지")] public string Lcz10 { get; set; } = "저위험군 봉쇄 10분 남음.";
+        [Description("저위험군 봉쇄 10분 메세지")]
+        public string Lcz10 { get; set; } = "<color=red>Alert</color>\n<size=30>LCZ lockdown 10min left</size>";
 
         [Description("저위험군 봉쇄 10분 메세지 시간(초단위)")]
         public ushort Lcz10Time { get; set; } = 10;
 
-        [Description("저위험군 봉쇄 5분 메세지")] public string Lcz5 { get; set; } = "저위험군 봉쇄 5분 남음.";
+        [Description("저위험군 봉쇄 5분 메세지")]
+        public string Lcz5 { get; set; } = "<color=red>Alert</color>\n<size=30>LCZ lockdown 5min left</size>";
 
         [Description("저위험군 봉쇄 5분 메세지 시간(초단위)")]
         public ushort Lcz5Time { get; set; } = 10;
 
-        [Description("저위험군 봉쇄 1분 메세지")] public string Lcz1 { get; set; } = "저위험군 봉쇄 1분 남음.";
+        [Description("저위험군 봉쇄 1분 메세지")]
+        public string Lcz1 { get; set; } = "<color=red>Alert</color>\n<size=30>LCZ lockdown 1min left</size>";
 
         [Description("저위험군 봉쇄 1분 메세지 시간(초단위)")]
         public ushort Lcz1Time { get; set; } = 10;
 
-        [Description("저위험군 봉쇄 30초 메세지")] public string Lcz30 { get; set; } = "저위험군 봉쇄 30초 남음.";
+        [Description("저위험군 봉쇄 30초 메세지")]
+        public string Lcz30 { get; set; } =
+            "<color=red>Alert</color>\n<size=30>LCZ lockdown 30sec left\n<color=red>Evacuate Immediately</color></size>";
 
         [Description("저위험군 봉쇄 30초 메세지 시간(초단위)")]
         public ushort Lcz30Time { get; set; } = 10;
 
-        [Description("저위험군 봉쇄 시작 메세지")] public string LcZstart { get; set; } = "저위험군 봉쇄 됨";
+        [Description("저위험군 봉쇄 시작 메세지")]
+        public string LcZstart { get; set; } =
+            "<size30>Light Containment Zone is locked down and ready for decontamination. \n<color=red>The removal of organic substances has now begun</color></size>";
 
         [Description("저위험군 봉쇄 시작 메세지 시간(초단위)")]
         public ushort LcZstarttime { get; set; } = 10;
 
-        [Description("저위험군 봉쇄 메세지를 오직 저위험군에만 띄울까요?(저위험군 봉쇄 시작은 제외)")]
+        [Description("Should low-risk containment messages only be posted to low-risk groups (except for the start of low-risk containment)")]
         public bool OnlyLcZinMessage { get; set; } = true;
 
-        [Description("NTF 지원 메세지({0} = MTF 유닛 이름, {1} = MTF 유닛 숫자, {2} = SCP-049-2를 제외한 재격리 대기중인 SCP 개체)")]
+        [Description("NTF Respawn Message({0} = MTF Unit Name, {1} = MTF Unit Number, {2} = SCP objects waiting for re-quarantine except SCP-049-2)")]
         public string NtfRespawn { get; set; } =
-            "<color=#1100ff>M</color><color=#3021ff>T</color><color=#4c40ff>F</color> {0}-{1}(이)가 지원이 왔습니다.\n재격리 대기 중인 SCP개체는 {2}마리입니다.";
+            "<color=#1100ff>M</color><color=#3021ff>T</color><color=#4c40ff>F</color> {0}-{1} entered the facility.\n Awaiting re-containment of {2} SCP subject(s).";
 
-        [Description("NTF 지원 메세지 시간")] public ushort NtfRespawntime { get; set; } = 10;
+        [Description("NTF Repawn Announcement Time")] public ushort NtfRespawntime { get; set; } = 10;
 
-        [Description("오늘의 공지사할 적기")]
+        [Description("Write today's announcement")]
         public string AnnouncmentMessage { get; set; } =
-            "<size=38><b><color=#6900ff>📣공</color><color=#7919ff>지</color><color=#852eff>사</color><color=#8f40ff>항📣</color></b></size>\n<size=34>오늘의 공시사항은 없습니다!\n즐거운 하루 되십시오!</size>";
-
+            "<size=38><b><color=#6900ff>📣An</color><color=#7919ff>no</color><color=#852eff>un</color><color=#8f40ff>cements📣</color></b></size>\n<size=34>There are no announcements today! \nHave a wonderful day!</size>";
     }
 }
