ddfb76eec54ce06b2ddab7fee8aa5b089a56e1be
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-06-19 00:58:03 +0900

    New Ability Shockwave

---
+++ GhostPlugin/Custom/Abilities/Active/Shockwave.cs
@@ -0,0 +1,30 @@
+using CustomPlayerEffects;
+using Exiled.API.Features;
+using Exiled.CustomRoles.API.Features;
+using GhostPlugin.Custom.Items.Grenades;
+
+namespace GhostPlugin.Custom.Abilities.Active
+{
+    public class Shockwave : ActiveAbility
+    {
+        public override string Name { get; set; } = "충격파";
+        public override string Description { get; set; } = "능력을 사용하여 충격파로 적들을 일시적으로 마비시킵니다!";
+        public override float Duration { get; set; } = 1f;
+        public override float Cooldown { get; set; } = 35f;
+
+        protected override void AbilityUsed(Player player)
+        {
+            var grenade = new StunGrenade();
+            var pickup = grenade.Throw(
+                position: player.Position,
+                force: 0f,
+                weight:0.1f,
+                grenadeType: grenade.Type,
+                fuseTime: 0.3f,
+                player: player
+            );
+            player.DisableEffect<Slowness>();
+            base.AbilityUsed(player);
+        }
+    }
+}
--- GhostPlugin/Custom/Roles/Scps/DwarfZombie.cs
+++ GhostPlugin/Custom/Roles/Scps/DwarfZombie.cs
@@ -1,6 +1,7 @@
 using CustomPlayerEffects;
 using Exiled.API.Features;
 using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Spawn;
 using Exiled.CustomRoles.API.Features;
 using PlayerRoles;
 using GhostPlugin.API;
@@ -19,6 +20,11 @@ namespace GhostPlugin.Custom.Roles.Scps
         public override string CustomInfo { get; set; } = "Dwarf SCP-049-2";
         public override RoleTypeId Role { get; set; } = RoleTypeId.Scp0492;
         public StartTeam StartTeam { get; set; } = StartTeam.Scp | StartTeam.Revived;
+
+        public override SpawnProperties SpawnProperties { get; set; } = new SpawnProperties()
+        {
+            Limit = 1,
+        };
 
         protected override void RoleAdded(Player player)
         {
