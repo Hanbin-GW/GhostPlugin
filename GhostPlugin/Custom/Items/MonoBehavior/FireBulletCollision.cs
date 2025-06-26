e36a1818b05de9b32c645eccdbe0543564154b43
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-06-26 18:00:13 +0900

    fix: repeated damage in customammo

--- GhostPlugin/Custom/Items/MonoBehavior/FireBulletCollision.cs
+++ GhostPlugin/Custom/Items/MonoBehavior/FireBulletCollision.cs
@@ -3,7 +3,6 @@ using Exiled.API.Enums;
 using Exiled.API.Features;
 using PlayerStatsSystem;
 using UnityEngine;
-using PlayerStatsSystem;
 
 
 namespace GhostPlugin.Custom.Items.MonoBehavior
@@ -12,11 +11,12 @@ namespace GhostPlugin.Custom.Items.MonoBehavior
     {
         private int _damage;
         private Player _attacker;
-
+        private bool _hasCollided = false;
         public void Initialize(int damage, Player attacker)
         {
             _damage = damage;
             _attacker = attacker;
+            Log.Debug($"[FireBullet] Initialized with damage: {damage}, attacker: {_attacker?.Nickname}");
         }
 
         private void OnCollisionEnter(Collision collision)
@@ -27,16 +27,19 @@ namespace GhostPlugin.Custom.Items.MonoBehavior
             {
                 if (target.Role.Team == _attacker.Role.Team)
                     return;
+                _hasCollided = true; 
+                if (target.Role.Team == _attacker.Role.Team)
+                    return;
                 Log.Debug($"Hit Player: {target.Nickname} - Damage: {_damage}");
                 target.EnableEffect<Burned>(duration: 5);
                 //target.Hurt(_damage, DamageType.E11Sr, _attacker.Nickname);
-                target.Hurt(new CustomReasonDamageHandler( "Hit on Burining pellet", _damage));
+                target.Hurt(new CustomReasonDamageHandler( "Buring bullet", _damage));
                 _attacker.ShowHitMarker();
                 Destroy(gameObject);
             }
             else
             {
-                Destroy(gameObject,2f);
+                Destroy(gameObject,3f);
             }
         }
     }
--- GhostPlugin/Custom/Items/MonoBehavior/PoisonBulletCollision.cs
+++ GhostPlugin/Custom/Items/MonoBehavior/PoisonBulletCollision.cs
@@ -9,34 +9,38 @@ namespace GhostPlugin.Custom.Items.MonoBehavior
     {
         private int damage;
         private Player _attacker;
-
+        private bool _hasCollided = false;
         public void Initialize(int damage, Player attacker)
         {
             this.damage = damage;
             _attacker = attacker;
+            Log.Debug($"[PoisonBullet] Initialized with damage: {damage}, attacker: {_attacker?.Nickname}");
         }
 
         private void OnCollisionEnter(Collision collision)
         {
+            if (_hasCollided) return;
+
             Player target = Player.Get(collision.collider) ?? Player.Get(collision.collider.GetComponentInParent<Collider>());
 
             if (target != null && target != _attacker)
             {
                 if (target.Role.Team == _attacker.Role.Team)
                     return;
+                _hasCollided = true;
                 Log.Debug($"Hit Player: {target.Nickname}");
                 
                 target.EnableEffect<Decontaminating>(duration: 8,intensity:1);
                 target.EnableEffect<Poisoned>(duration: 20,intensity:2);
                 target.Hurt(damage, DamageType.Com15, _attacker.Nickname);
-                target.ShowHint("<color=red>매우 심각한 방사선 염산물질에 노출되었습니다!</color>",10);
+                target.ShowHint("<color=red>You are exposed to very serious radiation hydrochloride!</color>",10);
                 _attacker.ShowHitMarker();
 
-                Destroy(gameObject, 4.5f);
+                Destroy(gameObject);
             }
             else
             {
-                Destroy(gameObject,1f);
+                Destroy(gameObject,4.5f);
             }
         }
     }
